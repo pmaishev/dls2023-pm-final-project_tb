813af00758f7e296db8d78f487647db3be0c2937
Petr Maishev <petr.maishev@spsr.tech>
2023-06-14 11:45:34 +0300

    WIP: style transfer

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ class CBotConfig():
     def __init__(self):
         self.data_saver = CFileDataSaver()
         self.token = os.environ["TG_BOT_TOKEN"]
-    
+
     def get_data_saver(self) -> IDataSaver:
         return self.data_saver
