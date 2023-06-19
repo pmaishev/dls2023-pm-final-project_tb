78e9c31f89b446a869789e7b55ce2b58c08d52be
Petr Maishev <petr.maishev@spsr.tech>
2023-06-19 09:16:54 +0300

    FIX: tests

--- tests/test.py
+++ tests/test.py
@@ -17,7 +17,7 @@ img = trans.transfer(links)
 img.show()
 digits = hashlib.md5(img.tobytes()).hexdigest()
 print(digits)
-#assert('d474c601a5c6204c4a1f560500b3c10a'==digits)
+assert('3bd537a6f59cd9bec6a0ab9b6bb546f7'==digits)
 
 links['content'] = './data_test/1/content_IMG_7359.jpg'
 links['style'] = './data_test/1/style_IMG_7580.jpg'
@@ -25,7 +25,7 @@ img = trans.transfer(links)
 img.show()
 digits = hashlib.md5(img.tobytes()).hexdigest()
 print(digits)
-# #assert('dceccefad2626f46e58b25c713c7b8bb'==digits)
+#assert('82a632cb74a72afcc419d7f7eb910b4e'==digits)
 
 links['content'] = './data_test/2/dancing.jpg'
 links['style'] = './data_test/2/picasso.jpg'
@@ -33,4 +33,4 @@ img = trans.transfer(links)
 img.show()
 digits = hashlib.md5(img.tobytes()).hexdigest()
 print(digits)
-#assert('dceccefad2626f46e58b25c713c7b8bb'==digits)
+assert('cf0568e2b3e5b905e9723c8631d7c80c'==digits)
